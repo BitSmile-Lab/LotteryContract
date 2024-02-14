a7d80d60c2c172194602a3c8213baf191b7f0117
0502lian <28177408@qq.com>
2024-02-14 11:52:29 +0800

    modify deploy

--- script/deployBlastLottery.s.sol
+++ script/deployBlastLottery.s.sol
@@ -8,7 +8,7 @@ contract BlastLotteryScript is Script {
     //lottery addres on sopelia is  0x02E4F2Ea0b540D0C4bb5a9D96892f442E1983D3E
 
     //blast testnet
-    address constant MOCK_TOKEN = 0x4200000000000000000000000000000000000023;
+    address constant AWARD_TOKEN = 0x4200000000000000000000000000000000000023;
     address constant GENERATOR = 0xC9c4BDD43cD9970BEA1206EA6e80825DE11C047D;
 
     function setUp() public {}
@@ -18,7 +18,7 @@ contract BlastLotteryScript is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        BlastLottery lottery = new BlastLottery(MOCK_TOKEN, GENERATOR);
+        BlastLottery lottery = new BlastLottery(AWARD_TOKEN, GENERATOR);
         console.log(address(lottery));
         vm.stopBroadcast();
     }
